d77db628535af01c5515303d48b45fa55d3d2514
Oleg20111 <lusenkooleh1@gmail.com>
2022-03-11 17:10:07 +0300

    performance fix Challenge 3 - Balloons, Bombs, & Booleans

--- Assets/Challenge 3/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 3/Scripts/PlayerControllerX.cs
@@ -7,8 +7,9 @@ public class PlayerControllerX : MonoBehaviour
     public bool gameOver;
 
     public float floatForce;
-    private float gravityModifier = 1.5f;
+    private float gravityModifier = 1f;
     private Rigidbody playerRb;
+    private bool outOnTheField = false;
 
     public ParticleSystem explosionParticle;
     public ParticleSystem fireworksParticle;
@@ -23,6 +24,7 @@ public class PlayerControllerX : MonoBehaviour
     {
         Physics.gravity *= gravityModifier;
         playerAudio = GetComponent<AudioSource>();
+        playerRb = GetComponent<Rigidbody>();
 
         // Apply a small upward force at the start of the game
         playerRb.AddForce(Vector3.up * 5, ForceMode.Impulse);
@@ -32,8 +34,19 @@ public class PlayerControllerX : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(transform.position.y > 14.4f)
+        {
+            transform.position = new Vector3(transform.position.x, 14.4f, transform.position.z);
+            playerRb.velocity = Vector3.zero;
+            outOnTheField = true;
+        }
+        else
+        {
+            outOnTheField = false;
+        }
+
         // While space is pressed and player is low enough, float up
-        if (Input.GetKey(KeyCode.Space) && !gameOver)
+        if (Input.GetKey(KeyCode.Space) && !gameOver && !outOnTheField) 
         {
             playerRb.AddForce(Vector3.up * floatForce);
         }
@@ -57,7 +70,11 @@ public class PlayerControllerX : MonoBehaviour
             fireworksParticle.Play();
             playerAudio.PlayOneShot(moneySound, 1.0f);
             Destroy(other.gameObject);
+        }
 
+        else if(other.gameObject.CompareTag("Ground"))
+        {
+            playerRb.AddForce(Vector3.up * 10, ForceMode.Impulse);
         }
 
     }
